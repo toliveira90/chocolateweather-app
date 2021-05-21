586e224176bd39a7fd3c7975cb58fa1170590673
toliveira90 <34128530+toliveira90@users.noreply.github.com>
2021-05-21 17:27:02 +0100

    Forecast added
    From API

--- src/app.js
+++ src/app.js
@@ -32,9 +32,8 @@ function formatDay(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-
   let forecastElement = document.querySelector("#forecast");
-
+console.log(response.data)
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
@@ -72,6 +71,8 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
+
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
